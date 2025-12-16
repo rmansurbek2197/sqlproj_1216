d6e9f6f72e0eb24ab361736c0f5fa9efe95068d5
rmansurbek2197 <rmansurbek2197@gmail.com>
2025-12-16 15:41:51 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,28 @@
+# 1
+CREATE TABLE talabalar (
+    yosh INTEGER,
+    ism TEXT,
+    gpa REAL,
+    telefon TEXT NULL
+);
+
+# 2
+CREATE TABLE mahsulotlar (
+    narx REAL,
+    tavsif TEXT
+);
+
+# 3
+CREATE TABLE xodimlar (
+    oylik REAL NULL
+);
+
+# 4
+CREATE TABLE kitoblar (
+    sahifalar_soni INTEGER
+);
+
+# 5
+CREATE TABLE foydalanuvchilar (
+    email TEXT NULL
+);
